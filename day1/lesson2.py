b64b7cc080d658e54e7a388f9fb7dc6203a74d3f
lasha22 <taziashvililasha20@gmail.com>
2024-01-13 18:34:42 +0400

    first commit

---
+++ day1/lesson2.py
@@ -0,0 +1,9 @@
+name= "lasha"
+surname= "taziashvili"
+nickname= "tazmena"
+age=15
+height=164.5
+weight=48.5
+
+print("My full name is" + " " + name + " " + surname + " " + "but my friends call me" + nickname + " " + "and I am" + str(age) + " yerars old, " + "my phsical data is: height " + str(height) + "cm" + "and weight " + str(weight)
++ "kg " + "i like video games")
