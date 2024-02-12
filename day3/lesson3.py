1b644bf00cdee59d187e867e6a762450fc38b759
lasha22 <taziashvililasha20@gmail.com>
2024-02-12 18:16:58 +0400

    fourth commit

---
+++ day3/lesson3.py
@@ -0,0 +1,26 @@
+motherage = 39
+print(motherage)
+
+fatherage = 48
+print(fatherage)
+
+sisterage = 11
+print(sisterage)
+
+myage= 15
+print(myage)
+
+
+print("After 2 years")
+motherage = 41
+print(motherage)
+
+fatherage = 51
+print(fatherage)
+
+sisterage = 13
+print(sisterage)
+
+myage = 17
+print(myage)
+
